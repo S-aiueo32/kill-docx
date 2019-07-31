a067840c8563ad179d0b3ad5de9e2d6ac74d1690
S-aiueo32 <s.aiueo32@gmail.com>
2019-07-31 18:03:35 +0900

    :+1: improve the codes and update README.

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
-# kill-docx
-Microsoft Word should be die.
+# :fu: kill-docx
+> Say NO to MS Word, Say YES to LaTeX.
 
-# Reference
+## Introduction
+These codes convert PDF to PNG and embed them to Word documents, in the other words, kill Microsoft Word.
+You can do any assignments with LaTeX even if `.docx` format is required.
+
+## Requirements
+Here works with `Python` with a few package below. I recommed to create virtual environment with `pipenv`.
+```
+Python 3.6
+ - python-docx
+ - pdf2image
+ - pillow
+```
+
+**[IMPORTANT]** `pdf2image` package requires to install `poppler` which is out of `pip` package managements. So check [here](https://github.com/Belval/pdf2image) out!
+
+## Usage
+Let's kill them! You will see a `docx` file like `./src/chicken.pdf`.
+```
+python main.py
+```
+You can set arguments below:
+```
+--input : PDF file to input.
+--output: Filepath to output docx file.
+--size: Paper size to output. choose from A0-A5, B0-B5, US Letter and Legal.
+--direction: Portrait mode or Landscape mode.
+--save_pages: Whether each page is saved in the directory.
+```
+
+## :chichen: Reference
 1. Zongker, Doug. "Chicken chicken chicken: Chicken chicken." Annals of Improbable Research 12 , no. 5 (2006): 16--21.
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 from pathlib import Path
 import argparse
 
@@ -5,40 +6,76 @@ from docx import Document
 from docx.shared import Mm
 from pdf2image import convert_from_path
 
-src_dir = Path('./src')
 
-def save_pages(filename):
-	pages = convert_from_path(str(src_dir / filename))
-	out_files = []
-	for i, page in enumerate(pages):
-		out_file = src_dir / f'{filename.replace(".pdf", "")}_{i:0>4}.png'
-		out_files.append(out_file)
-		page.save(out_file)
-	return out_files
+PAPER_SIZE = {
+    # A papers
+    'a0': {'short': 841, 'long': 1189},
+    'a1': {'short': 594, 'long': 841},
+    'a2': {'short': 420, 'long': 594},
+    'a3': {'short': 297, 'long': 420},
+    'a4': {'short': 210, 'long': 297},
+    'a5': {'short': 148, 'long': 210},
+    # B papers
+    'b0': {'short': 1000, 'long': 1414},
+    'b1': {'short': 707, 'long': 1000},
+    'b2': {'short': 500, 'long': 707},
+    'b3': {'short': 353, 'long': 500},
+    'b4': {'short': 250, 'long': 353},
+    'b5': {'short': 176, 'long': 250},
+    # US paper size
+    'letter': {'short': 216, 'long': 279},
+    'legal': {'short': 216, 'long': 356}
+}
+
+
+def add_pages(doc, path, save_pages=False):
+    pages = convert_from_path(str(path))
+    for i, page in enumerate(pages):
+        if args.save_pages:
+            name_ = path.name.replace('.pdf', f'_{i:0>4}.png')
+            output = str(path.with_name(name_))
+        else:
+            output = BytesIO()
+        page.save(output, 'PNG')
+        doc.add_picture(output, width, height)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, default='./src/chicken.pdf')
+    parser.add_argument('--output', type=str, default='./output.docx')
+    parser.add_argument('--size', default='a4', choices=PAPER_SIZE.keys())
+    parser.add_argument('--direction', default='portrait', choices=['portrait', 'landscape'])
+    parser.add_argument('--save_pages', action='store_true', default=False)
+    args = parser.parse_args()
+
+	# store args
+    input_path = Path(args.input)
+    output_path = Path(args.output)
+    size = PAPER_SIZE[args.size]
+    if args.direction == 'portrait':
+        width, height = Mm(size['short']), Mm(size['long'])
+    else:
+        width, height = Mm(size['long']), Mm(size['short'])
+
+	# instantiation docx
+    doc = Document()
+
+	# set parameters
+	section, *_ = doc.sections  # store 1st section
+	section.bottom_margin = 0
+	section.left_margin = 0
+	section.right_margin = 0
+	section.top_margin = 0
+    section.page_width = width
+    section.page_height = height
+
+	# convert and add pages to docx
+    add_pages(doc, input_path, args.save_pages)
+
+	# save
+    doc.save(output_path)
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--filename', type=str, default='chicken.pdf')
-	args = parser.parse_args()
-
-	doc = Document()
-
-	sections = doc.sections
-	for section in sections:
-		section.top_margin = 0
-		section.bottom_margin = 0
-		section.left_margin = 0
-		section.right_margin = 0
-		section.page_height = Mm(297)
-		section.page_width = Mm(210)
-
-		
-	p = doc.add_paragraph()	
-	r = p.add_run()
-
-	image_paths = save_pages(args.filename)
+	main()
 	
-	for image_path in image_paths:
-		r.add_picture(str(image_path), height=Mm(297), width=Mm(210))
-
-	doc.save('./output.docx')
